dd56e0e04e965febdd25a85b1ce01b1f05e113f4
Matheus José Ferreira de Aguiar <matheusjfa@outlook.com>
2024-09-21 07:09:01 -0300

    Refactor: Adding more comments in the code

--- src/main/java/com/matheusjfa/design/patterns/Creational/Singleton.java
+++ src/main/java/com/matheusjfa/design/patterns/Creational/Singleton.java
@@ -4,7 +4,6 @@ package com.matheusjfa.design.patterns.Creational;
  * The Singleton Pattern ensures that a class has only one instance and provides a global point of access to it.
  * In this case, the Singleton Pattern is used to create a single instance of the Singleton class.
  * The Database class is the Singleton class, and the instance method is the Singleton Method.
- * HashCode is used to compare the instances.
  * The main method is used to create a single instance of the Singleton class.
  *
  * Output:
@@ -16,6 +15,9 @@ public class Singleton {
         var anotherDatabaseAccess = Database.getInstance();
 
         System.out.println("Equality: " + database.equals(anotherDatabaseAccess));
+
+        System.out.println(database);
+        System.out.println(anotherDatabaseAccess);
     }
 }
 
@@ -36,8 +38,8 @@ class Database {
         if (this == obj) return true; // Verify if the objects have the same reference
         if (obj == null || getClass() != obj.getClass()) return false; // Verify if the objects are from the same class
 
-        final Database database = (Database) obj;
-        return this.hashCode() == database.hashCode();
+        final Database database = (Database) obj; // Cast the object to the class type
+        return this.hashCode() == database.hashCode(); // Verify if the objects have the same hash code
     }
 
     @Override
